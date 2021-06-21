f2eb3dbdc96c3df26715328594705816f0e46647
Anzar Shaikh <anzarshaikh373@gmail.com>
2021-06-21 14:46:21 +0530

    Employee attendenc done.

--- empWage.java
+++ empWage.java
@@ -1,7 +1,23 @@
 package com.bl.assignment;
 
+import java.util.Random;
+import java.util.Random;
 public class empWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation. ");
+        Random rand = new Random();
+
+        int present = 1;
+        int absent = 2;
+        int check=rand.nextInt(3)+1;
+
+        if (check==present) {
+            System.out.println("Employee is present.");
+        }
+        else {
+            System.out.println("Employee is absent.");
+        }
+
+
     }
 }
