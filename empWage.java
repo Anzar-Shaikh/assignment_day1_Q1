128aef098c71bca7d24459f204949d48b59cb640
Anzar Shaikh <anzarshaikh373@gmail.com>
2021-06-21 14:54:05 +0530

    part time wages added.

--- empWage.java
+++ empWage.java
@@ -9,13 +9,19 @@ public class empWage {
 
         int present = 1;
         int absent = 2;
+        int part_time_present = 3;
         int check=rand.nextInt(3)+1;
         int wage_per_hr = 20;
         int full_day_hr = 8;
+        int part_time_hr = 4;
 
-        if (check==present) {
+        if (check == present) {
             System.out.println("Employee is present.");
         }
+        else if (check == part_time_present) {
+            System.out.println("Employee is present for part time.");
+        }
+
         else {
             System.out.println("Employee is absent.");
         }
